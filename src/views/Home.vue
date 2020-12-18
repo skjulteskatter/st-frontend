b17f4e18ca312f7da0a111c9cf80cd9ed9142a9a
Fredrik Vedvik <fredrik.ved@gmail.com>
2020-12-18 11:56:03 +0100

    add login button to receive token

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+        <button @click="login">LOGIN</button>
+    </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import api from '@/services/api';
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld,
+    },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    async login() {
+        await api.session.login();
+    }
+}
 </script>
