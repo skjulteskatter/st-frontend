30bff89adfb42ae640ac989048f52b748dcc2afa
Fredrik Vedvik <fredrik.ved@gmail.com>
2020-12-19 17:36:12 +0100

    dynamic basepath

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,13 @@
+let basePath = '/'
+
+console.log(process.env);
+
+if (process.env.NODE_ENV == "development") {
+    basePath = 'https://dmb-app.azurewebsites.net/'
+}
+
 export default {
     api: {
-        basePath: '/'
+        basePath: basePath
     }
 }
