5cc98d5cc6658961a06611bed04bec4352e1e2d8
Sigve Hansen <sighan12@gmail.com>
2021-01-08 11:04:44 +0100

    splitting into components

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="song-list">
-
+        <span class="song-list__item hover" v-for="item in items" :key="item.id">
+            {{ item }}
+        </span>
     </div>
 </template>
 
@@ -13,7 +15,7 @@ import { Options, Vue } from 'vue-class-component'
             type: Array,
             default: []
         }
-    }
+    },
 })
 export default class SongList extends Vue {
     
@@ -21,5 +23,21 @@ export default class SongList extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.song-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: calc(var(--spacing) * 0.5);
 
+    &__item {
+        background-color: white;
+        border: 1px solid #d0d0d0;
+        border-radius: 5px;
+        padding: calc(var(--spacing) * 0.2) var(--spacing);
+        cursor: pointer;
+
+        &:hover {
+            background-color: #d0d0d0;
+        }
+    }
+}
 </style>
---
+++ src/components/Songbooks.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="songbooks">
+      <card
+        v-for="songbook in collections"
+        :key="songbook.id"
+        class="songbooks__book hover"
+        :image="songbook.image"
+        border
+      >
+        <h2 class="songbooks__book__title">{{ songbook.name.no }}</h2>
+      </card>
+    </div>
+</template>
+
+<script>
+import { Options, Vue } from "vue-class-component";
+import Card from '@/components/Card.vue'
+
+@Options({
+    components: {
+        Card,
+    },
+    props: {
+        collections: {
+            type: Array,
+            default: []
+        }
+    }
+})
+export default class Songbooks extends Vue {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.songbooks {
+  display: grid;
+  gap: var(--spacing);
+  grid-template-columns: repeat(4, 1fr);
+
+  &__book {
+    grid-column: span 1;
+    transition: all 0.2s ease;
+    cursor: pointer;
+
+    &__title {
+      margin: 0;
+    }
+  }
+}
+</style>
