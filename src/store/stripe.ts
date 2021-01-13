7c5bc88e9f6a67fdf958a347753622baaffa735c
Fredrik Vedvik <fredrik@vedvik.tech>
2021-01-13 09:13:42 +0100

    stripe mutations

--- src/store/stripe.ts
+++ src/store/stripe.ts
@@ -1,3 +1,4 @@
+import stripeService from "@/services/stripe";
 import { SetupResponseCollection } from "checkout";
 import { InjectionKey } from "vue";
 import { createStore } from "vuex";
@@ -7,6 +8,7 @@ interface StripeStore {
         name: string;
         id: string;
     }[];
+    publicKey: string;
 }
 
 
@@ -15,11 +17,27 @@ export const stripeKey: InjectionKey<StripeStore> = Symbol();
 
 export const stripeStore = createStore<StripeStore>({
     state: {
-        products: []
+        products: [],
+        publicKey: ''
     },
     mutations: {
         setProducts(state, collections: SetupResponseCollection[]) {
             state.products = collections;
+        },
+        setKey(state, key: string) {
+            state.publicKey = key;
+        }
+    },
+    actions: {
+        async setup({ commit }) {
+            const result = await stripeService.setup();
+            await stripeService.init(result.key);
+
+            commit('setKey', result.key);
+            commit('setProducts', result.collections);
+        },
+        async startSession(state, priceId: string) {
+            await stripeService.checkout(priceId);
         }
     }
 })
