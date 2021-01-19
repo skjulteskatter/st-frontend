16fa89bf7726c6be45a6dd5d3ab9aac0027a181f
Fredrik Vedvik <fredrik@vedvik.tech>
2021-01-19 15:05:44 +0100

    tokens

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -28,34 +28,35 @@ class Auth {
     public async getToken() {
         if (this.token) return this.token;
         this.setToken(await firebase.getToken() ?? '');
+        return this.token;
     }
 
     public get isAuthenticated() {
-        this.accessToken = this.accessToken || localStorage.getItem('id_token') || ''
+        this.accessToken = this.accessToken || localStorage.getItem('id_token') || '';
         if (!this.accessToken) {
-            return false
+            return false;
         }
 
         // Check whether the current time is past the
         // access token's expiry time
-        const expiresAt = this.expiresAt || window.localStorage.getItem('id_expires_at') || ''
-        const authenticated = new Date().getTime() < expiresAt
-        return authenticated
+        const expiresAt = this.expiresAt || localStorage.getItem('id_expires_at') || '';
+        const authenticated = new Date().getTime() < expiresAt;
+        return authenticated;
     }
 
     public get token() {
         const expires =
             this.expiresAt ||
-            parseInt(window.localStorage.getItem('id_expires_at') || '0');
+            parseInt(localStorage.getItem('id_expires_at') || '0');
         if (expires > Date.now()) {
             const accessToken =
                 this.accessToken ||
-                window.localStorage.getItem('id_token')
+                localStorage.getItem('id_token');
             if (accessToken) {
-                return accessToken
+                return accessToken;
             }
         }
-        return null
+        return null;
     }
 }
 
