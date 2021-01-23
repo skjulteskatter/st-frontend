592e05d323e7b2aed049143b49606852759a771e
Sigve Hansen <sighan12@gmail.com>
2021-01-23 23:09:36 +0100

    designing the footer component

--- src/components/home/FooterComponent.vue
+++ src/components/home/FooterComponent.vue
@@ -1,7 +1,25 @@
 <template>
-    <footer id="footer">
-        <div class="container">
-            <small>Copyright 2021 © Stiftelsen Skjulte Skatters Forlag</small>
+    <footer class="footer">
+        <div class="container footer__content">
+            <div class="footer__content-group">
+                <b class="footer__content-group__title">Links</b>
+                <router-link to="/" class="footer__content-group__link">Home</router-link>
+                <router-link to="/blog" class="footer__content-group__link">Blog</router-link>
+            </div>
+            <div class="footer__content-group">
+                <b class="footer__content-group__title">Contact</b>
+                <router-link to="/contact" class="footer__content-group__link">Get in touch</router-link>
+            </div>
+            <div class="footer__content-group">
+                <b class="footer__content-group__title">Get the app</b>
+                <img src="/img/app_store_badge.png">
+                <img src="/img/play_store_badge.png">
+            </div>
+        </div>
+        <div class="footer__copyright">
+            <div class="container">
+                <small>Copyright 2021 © Stiftelsen Skjulte Skatters Forlag</small>
+            </div>
         </div>
     </footer>
 </template>
@@ -15,11 +33,39 @@ export default class FooterComponent extends Vue {
 </script>
 
 <style lang="scss">
-#footer {
+.footer {
     width: 100%;
 
     background: var(--secondary-background-color);
     border-top: 1px solid var(--border-color);
-    padding: var(--spacing);
+
+    &__copyright {
+        background: rgba(black, .1);
+    }
+
+    &__content {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: var(--spacing);
+
+        &-group {
+            display: flex;
+            flex-direction: column;
+            gap: .5em;
+
+            &__title {
+                margin-bottom: var(--spacing);
+            }
+
+            &__link {
+                color: var(--text-color);
+                text-decoration: none;
+            }
+
+            img {
+                max-width: 150px;
+            }
+        }
+    }
 }
 </style>
