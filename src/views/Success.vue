a503dd739edbe405f6b816795bfc98666e959dd8
Fredrik Vedvik <fredrik@vedvik.tech>
2021-01-26 09:36:21 +0100

    success redirect

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -3,3 +3,13 @@
         <h1>Success. Your purchase should be available soon.</h1>
     </div>
 </template>
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+
+export default class Success extends Vue {
+    public mounted() {
+        setTimeout(() => this.$router.push('/dashboard'), 5000);
+    }
+}
+</script>
