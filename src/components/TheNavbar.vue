c4c3f295304255c2d4a973c1403d061ff4ad240a
Sigve Hansen <sighan12@gmail.com>
2021-01-28 09:38:09 +0100

    updating navbar component

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,115 @@
+<template>
+	<nav class="nav" @click="toggleBurgerMenu">
+        <div class="container nav__wrapper">
+            <img class="nav__logo" :src="logo" @click="$router.push('/')"/>
+			<button secondary v-if="isMobileDevice">Menu</button>
+            <div class="nav__links" :class="{'nav__links-active': openBurgerMenu}">
+				<router-link class="nav__item" :to="{name: 'main'}">{{ $t('common.dashboard') }}</router-link>
+				<router-link class="nav__item" :to="{name: 'collections'}">{{ $t('common.songs') }}</router-link>
+				<router-link class="nav__item" :to="{name: 'store'}">{{ $t('common.store') }}</router-link>
+				<router-link v-if="isAdmin" class="nav__item" :to="{name: 'admin'}">Admin</router-link>
+				<settings class="nav__item settings"></settings>
+			</div>
+        </div>
+    </nav>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component"
+import Settings from '@/components/Options.vue';
+
+@Options({
+	components: {
+		Settings,
+	},
+	computed: {
+		isMobileDevice(){
+			return window.orientation != undefined
+		}
+	}
+})
+export default class TheNavbar extends Vue {
+	public openBurgerMenu = false
+
+	public get logo(){
+        // Return the direct url to the logo
+        return 'https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo3.jpg'
+	}
+
+	public toggleBurgerMenu(){
+		this.openBurgerMenu = !this.openBurgerMenu
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.nav {
+	background-color: var(--st-background-color);
+    border-bottom: 1px solid var(--st-border-color);
+
+    width: 100%;
+    display: flex;
+    justify-content: center;
+	z-index: 1;
+	
+	&__wrapper {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	&__links {
+		display: flex;
+		align-items: center;
+		gap: var(--st-spacing);
+	}
+
+	&__logo {
+		max-height: 35px;
+		cursor: pointer;
+	}
+
+	&__item {
+        color: var(--st-text-color);
+        text-decoration: none;
+		font-weight: bold;
+
+        &.router-link-exact-active {
+			color: var(--st-primary-color);
+			position: relative;
+
+			&:after {
+				content: '';
+				background-color: var(--st-primary-color);
+				position: absolute;
+				top: 100%;
+				left: 0;
+				height: 2px;
+				width: 100%;
+				animation: growFromLeft .3s ease;
+			}
+        }
+	}
+	
+	@media screen and (max-width: 600px) {
+		.nav__links {
+			padding: var(--st-spacing);
+			background-color: var(--st-background-color);
+			border-left: 1px solid var(--st-border-color);
+			flex-direction: column;
+			position: fixed;
+			height: 100%;
+			top: 0;
+			right: -100%;
+			transition: all .5s ease;
+
+			&-active {
+				right: 0;
+			}
+
+			.settings {
+				margin-top: auto;
+			}
+		}
+	}
+}
+</style>
--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -1,14 +1,5 @@
 <template>
-    <nav class="nav" v-if="user && initialized">
-        <div class="container nav__wrapper">
-            <img id="logo" :src="logo" @click="$router.push('/')"/>
-            <router-link class="nav__item" :to="{name: 'main'}">{{$t('common.dashboard')}}</router-link>
-            <router-link class="nav__item" :to="{name: 'collections'}">{{$t('common.songs')}}</router-link>
-            <router-link class="nav__item" :to="{name: 'store'}">{{$t('common.store')}}</router-link>
-            <router-link v-if="isAdmin" class="nav__item" :to="{name: 'admin'}">Admin</router-link>
-            <settings class="nav__item"></settings>
-        </div>
-    </nav>
+    <the-navbar v-if="user && initialized"></the-navbar>
     <div class="container" v-if="user && initialized">
         <router-view/>
     </div>
@@ -17,23 +8,18 @@
 import { Vue, Options } from 'vue-class-component';
 import { useStore } from 'vuex';
 import { sessionKey } from '../store';
-import Settings from '@/components/Options.vue';
 import themes from '@/classes/themes';
+import TheNavbar from '@/components/TheNavbar.vue';
 
 @Options({
     components: {
-        Settings,
+        TheNavbar
     }
 })
 export default class DashboardLayout extends Vue {
     mounted(){
         document.documentElement.style.setProperty('--st-primary-color', localStorage.getItem('theme_color') || themes.default);
         themes.load();
-    }
-
-    public get logo(){
-        // Return the direct url to the logo
-        return 'https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo3.jpg'
     }
 
     public get user() {
@@ -47,10 +33,4 @@ export default class DashboardLayout extends Vue {
     }
 }
 </script>
-<style lang="scss">
-#logo {
-    margin: 0 auto 0 0;
-    max-height: 35px;
-    cursor: pointer;
-}
-</style>
+<style lang="scss"></style>
