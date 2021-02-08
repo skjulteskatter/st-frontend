d704495fd8bdac6edcd0fefd8086243e0ee4d8bb
Fredrik Vedvik <fredrik@vedvik.tech>
2021-02-08 10:00:39 +0100

    fixed a bit of configuration

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,8 @@ if (process.env.NODE_ENV == "development") {
 
 export const firebaseConfig = {
     apiKey: 'AIzaSyC6b3XqiSM6uBuDq23THGzIWj6OgIogB_U',
-    authDomain: 'landax-music-app.firebaseapp.com',
-    databaseURL: 'https://landax-music-app.firebaseio.com',
+    authDomain: 'login.songtreasure.app',
     projectId: 'landax-music-app',
-    storageBucket: 'landax-music-app.appspot.com',
     messagingSenderId: '230724783358',
     appId: '1:230724783358:web:2642da04c8647c75e6a9af',
     measurementId: 'G-PP8F4ZZ3M0',
