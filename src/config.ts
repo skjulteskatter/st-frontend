6d2ccb3991d44b168d604482e84978df635f053b
Fredrik Vedvik <fredrik@vedvik.tech>
2021-02-08 10:01:04 +0100

    fixed

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,7 @@ export const firebaseConfig = {
     apiKey: 'AIzaSyC6b3XqiSM6uBuDq23THGzIWj6OgIogB_U',
     authDomain: 'login.songtreasure.app',
     projectId: 'landax-music-app',
+    storageBucket: 'landax-music-app.appspot.com',
     messagingSenderId: '230724783358',
     appId: '1:230724783358:web:2642da04c8647c75e6a9af',
     measurementId: 'G-PP8F4ZZ3M0',
