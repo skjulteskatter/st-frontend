0e8ca977a6f5be452a4f50f4f8b9455364dbf4cc
Sigve Hansen <sighan12@gmail.com>
2021-02-10 14:21:32 +0100

    making admin page look good

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,17 +3,29 @@
         <div v-if="isAdmin">
             <h1 class="admin-panel__title">Admin</h1>
             <hr />
-            <div class="admin-collections">
-                <div v-for="collection in collections" :key="collection.id">
-                    {{collection.key}}
-                    <button v-if="!disabled.includes(collection.id)" @click="clearCollection(collection.id)">CLEAR CACHE</button>
-                </div>
-            </div>
+            <base-card class="admin-panel__collections">
+                <base-card
+                    v-for="collection in collections"
+                    :key="collection.id"
+                    class="admin-panel__collections__item gap-y"
+                    border
+                    secondary
+                >
+                    <b>{{ collection.key }}</b>
+                    <base-button
+                        v-if="!disabled.includes(collection.id)"
+                        :action="() => clearCollection(collection.id)"
+                        theme="warning"
+                    >
+                        {{ $t("admin.clearcache") }}
+                    </base-button>
+                </base-card>
+            </base-card>
 
             <users-list :users="users" :currentUser="currentUser"></users-list>
         </div>
         <div v-else>
-            <h1>You don't have access to view this page</h1>
+            <h1>{{ $t("admin.noaccess") }}</h1>
         </div>
     </div>
 </template>
@@ -23,11 +35,15 @@ import { sessionKey, usersKey } from "@/store";
 import { Options, Vue } from "vue-class-component";
 import { useStore } from "vuex";
 import UsersList from "@/components/UsersList.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import BaseCard from "@/components/BaseCard.vue";
 import api from "@/services/api";
 
 @Options({
     components: {
         UsersList,
+        BaseButton,
+        BaseCard,
     },
 })
 export default class Subscriptions extends Vue {
@@ -61,6 +77,19 @@ export default class Subscriptions extends Vue {
 
 <style lang="scss">
 .admin-panel {
+    &__collections {
+        .card__content {
+            display: flex;
+            gap: var(--st-spacing);
+        }
+
+        &__item {
+            .card__content {
+                display: flex;
+                flex-direction: column;
+            }
+        }
+    }
     &__title {
         margin: 0;
     }
