6b030e343a4ff7ffa1b63276dbebf57f941c3cd7
Fredrik Vedvik <fredrik@vedvik.tech>
2021-02-10 09:24:23 +0100

    unused variable

--- src/classes/lyrics.ts
+++ src/classes/lyrics.ts
@@ -5,21 +5,6 @@ type Content = {
     };
 }
 
-const transpositions = {
-    'C': 0,
-    'C#': 1,
-    'D': 2,
-    'Eb': 3,
-    'E': 4,
-    'F': 5,
-    'F#': 6,
-    'G': 7,
-    'G#': 8,
-    'A': 9,
-    'Bb': 10,
-    'B': 11,
-}
-
 export class Lyrics implements LyricsInterface {
     number: number;
     title: string;
