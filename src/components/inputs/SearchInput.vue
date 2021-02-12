db2dacb4d6ae8e9724ca6698d2478f045eb76923
Sigve Hansen <sighan12@gmail.com>
2021-02-12 15:09:44 +0100

    starting work on search-input component

---
+++ src/components/inputs/SearchInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="search-input">
+        <input type="text" class="search-input__input" />
+        <base-button theme="secondary" class="search-input__button">{{
+            $t("common.search")
+        }}</base-button>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { BaseButton } from "@/components";
+
+@Options({
+    components: {
+        BaseButton,
+    },
+})
+export default class SearchInput extends Vue {}
+</script>
+
+<style lang="scss">
+.search-input {
+    &__button {
+        border-left: none !important;
+        border-radius: 0px var(--st-border-radius) var(--st-border-radius) 0px !important;
+    }
+    &__input {
+        border-radius: var(--st-border-radius) 0px 0px var(--st-border-radius) !important;
+    }
+}
+</style>
--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -1,9 +1,11 @@
 import ButtonGroup from './ButtonGroup.vue'
 import BaseDropdown from './BaseDropdown.vue'
 import SongFilterDropdown from './SongFilterDropdown.vue'
+import SearchInput from './SearchInput.vue'
 
 export {
 	ButtonGroup,
 	BaseDropdown,
 	SongFilterDropdown,
+	SearchInput,
 }
