7098fb5a01f9e42c7eecedfc5f115f325f2df1f8
Fredrik Vedvik <fredrik@vedvik.tech>
2021-02-12 13:05:15 +0100

    auth domain

--- src/config.ts
+++ src/config.ts
@@ -6,9 +6,8 @@ if (process.env.NODE_ENV == "development") {
 
 export const firebaseConfig = {
     apiKey: 'AIzaSyC6b3XqiSM6uBuDq23THGzIWj6OgIogB_U',
-    authDomain: 'login.songtreasure.app',
+    authDomain: 'songtreasure.app',
     projectId: 'landax-music-app',
-    storageBucket: 'landax-music-app.appspot.com',
     messagingSenderId: '230724783358',
     appId: '1:230724783358:web:2642da04c8647c75e6a9af',
     measurementId: 'G-PP8F4ZZ3M0',
