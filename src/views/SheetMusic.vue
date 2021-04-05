53fdac5696682447a27564c7b4b0d4e763717387
Sigve Hansen <sighan12@gmail.com>
2021-04-05 09:48:42 +0200

    refactor

--- src/views/SheetMusic.vue
+++ src/views/SheetMusic.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
-        <open-sheet-music-display
-            :url="url"
-            :originalKey="originalKey"
-            :initialTransposition="transposition"
-            :embed="embed"
-            :initialZoom="zoom"
-        ></open-sheet-music-display>
-    </div>
+    <open-sheet-music-display
+        :url="url"
+        :originalKey="originalKey"
+        :initialTransposition="transposition"
+        :embed="embed"
+        :initialZoom="zoom"
+    ></open-sheet-music-display>
 </template>
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import OpenSheetMusicDisplay from "@/components/OSMD.vue"; 
+import OpenSheetMusicDisplay from "@/components/OSMD.vue";
 
 @Options({
     components: {
         OpenSheetMusicDisplay,
-    }
+    },
 })
 export default class SheetMusic extends Vue {
     public searchParams = new URLSearchParams(window.location.search);
@@ -26,7 +24,10 @@ export default class SheetMusic extends Vue {
     }
 
     public get originalKey() {
-        return this.searchParams.get("originalKey")?.replace("sharp", "#").replace("flat", "b");
+        return this.searchParams
+            .get("originalKey")
+            ?.replace("sharp", "#")
+            .replace("flat", "b");
     }
 
     public get transposeKey() {
@@ -40,7 +41,7 @@ export default class SheetMusic extends Vue {
     public get embed() {
         const query = this.searchParams.get("embed");
 
-        const embed = ['', "true"].includes(query ?? 'false');
+        const embed = ["", "true"].includes(query ?? "false");
 
         return embed;
     }
@@ -48,7 +49,7 @@ export default class SheetMusic extends Vue {
     public get zoom() {
         const query = this.searchParams.get("zoom");
 
-        const zoom = query ? parseInt(query)/100 : undefined;
+        const zoom = query ? parseInt(query) / 100 : undefined;
 
         return zoom;
     }
