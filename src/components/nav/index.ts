23cd05841ae17ab67176ab6c5dbf7ab24a6421cb
Sigve Hansen <sighan12@gmail.com>
2021-04-06 09:23:30 +0200

    extract logo to component

--- src/components/nav/index.ts
+++ src/components/nav/index.ts
@@ -1,9 +1,11 @@
 import CollectionList from './CollectionList.vue'
 import UserProfile from './UserProfile.vue'
 import NavLinks from './NavLinks.vue'
+import NavLogo from './NavLogo.vue'
 
 export {
 	CollectionList,
 	UserProfile,
 	NavLinks,
+	NavLogo,
 }
