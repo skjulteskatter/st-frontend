c7b65b95406de9aea1b8049b8bedeb64e284a231
Fredrik Vedvik <fredrik@vedvik.tech>
2021-04-23 11:11:26 +0200

    indent eslint

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -1,9 +1,7 @@
-/** eslint */
+/** eslint-ignore */
 import { IDBPDatabase, openDB } from "idb"; 
 
-const store = "songtreasures";
-
-export class CacheService<T> {
+class CacheService<T> {
     private name: string;
     private _db?: IDBPDatabase<unknown>;
 
@@ -12,42 +10,44 @@ export class CacheService<T> {
     }
 
     public async init() {
-        this._db ??= await openDB(store);
+        const name = this.name;
+        this._db ??= await openDB("songtreasures", 2, {
+            upgrade(db, odb, version, tx) {
+                if (!db.objectStoreNames.contains(name)) {
+                    db.createObjectStore(name);
+                }
+            },
+        });
+        return this._db;
     }
 
-    public get db() {
+    public async db() {
         if (this._db) return this._db;
-
-        throw new Error("DB NOT PRESENT");
+        return await this.init();
     }
 
-    public get(key: string): Promise<T | undefined> {
-        const transaction = this.db
-
-        return this.db?.get(this.name, key) as Promise<T | undefined>;
+    public async get(key: string): Promise<T | undefined> {
+        return (await this.db()).get(this.name, key) as Promise<T | undefined>;
     }
 
-    public async getAll(): Promise<{
-        [key: string]: T;
-    }> {
-        const transaction = this.db?.transaction(this.name);
+    public async getAll(): Promise<T[]> {
+        const transaction = (await this.db()).transaction(this.name);
 
         const result = await transaction?.objectStore(this.name).getAll() ?? [];
-        const keys = await transaction?.objectStore(this.name).getAllKeys() ?? [];
 
-        return keys.reduce((a, b) => a[b.toString()] = result[keys.indexOf(b)], {} as {
-            [key: string]: T;
-        });
+        await transaction.done;
+
+        return result as T[];
     }
 
     public async set(key: string, value: T): Promise<void> {
-        await this.db?.put(this.name, value, key);
+        await (await this.db())?.put(this.name, value, key);
     }
 
     public async setAll(value: {
         [key: string]: T;
     }) {
-        const tx = this.db?.transaction(this.name, "readwrite");
+        const tx = (await this.db()).transaction(this.name, "readwrite");
 
         for (const key of Object.keys(value)) {
             await tx?.objectStore(this.name).add(value[key], key);
@@ -104,3 +104,8 @@ export class CacheService<T> {
 // export class CacheService<T> {
 
 // }
+
+
+export {
+    CacheService,
+};
