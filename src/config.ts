62a3ee142e22bead24b85283844f4a898d9ba4f7
Fredrik Vedvik <fredrik@vedvik.tech>
2021-05-12 11:03:39 +0200

    new firebase

--- src/config.ts
+++ src/config.ts
@@ -7,13 +7,22 @@ if (process.env.NODE_ENV == "development") {
 }
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyC6b3XqiSM6uBuDq23THGzIWj6OgIogB_U",
-    authDomain: "songtreasure.app",
-    projectId: "landax-music-app",
-    messagingSenderId: "230724783358",
-    appId: "1:230724783358:web:2642da04c8647c75e6a9af",
-    measurementId: "G-PP8F4ZZ3M0",
+    apiKey: "AIzaSyDTZsQJxCT3w7cIQzYJ6T3YxNLIF-2LEpk",
+    authDomain: "songtreasures.firebaseapp.com",
+    projectId: "songtreasures",
+    storageBucket: "songtreasures.appspot.com",
+    messagingSenderId: "721098499416",
+    appId: "1:721098499416:web:fc3d22660728b82e82f12d",
 };
+
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyC6b3XqiSM6uBuDq23THGzIWj6OgIogB_U",
+//     authDomain: "songtreasure.app",
+//     projectId: "landax-music-app",
+//     messagingSenderId: "230724783358",
+//     appId: "1:230724783358:web:2642da04c8647c75e6a9af",
+//     measurementId: "G-PP8F4ZZ3M0",
+// };
 
 export default {
     api: {
