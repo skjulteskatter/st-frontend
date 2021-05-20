2c7ee59b214fac100c2a4712c4b55b86f7281ae9
Sigve Hansen <sighan12@gmail.com>
2021-05-20 16:16:11 +0200

    move notifications to bottom right

--- src/components/notification/NotificationGroup.vue
+++ src/components/notification/NotificationGroup.vue
@@ -34,7 +34,7 @@ export default class NotificationGroup extends Vue {
 <style lang="scss">
 .notification-group {
     position: fixed;
-    top: 0;
+    bottom: 0;
     right: 0;
     padding: var(--st-spacing);
     display: flex;
