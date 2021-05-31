8451d2e6c0a516401a88f7055ecffe6f3b2844a7
Sigve Hansen <sighan12@gmail.com>
2021-05-31 09:27:30 +0200

    add dropdown animation

--- src/components/inputs/BaseDropdown.vue
+++ src/components/inputs/BaseDropdown.vue
@@ -12,14 +12,26 @@
             </button>
             <slot name="button" v-else></slot>
         </div>
-        <base-card v-if="show" :class="`absolute top-11 ${origin == 'left' ? 'left-0 right-auto' : 'right-0 left-auto'}`">
-            <slot name="default"></slot>
-        </base-card>
+        <transition-root 
+            :show="show"
+            as="template"
+            enter="transition transform"
+            enter-from="-translate-y-2 opacity-0"
+            enter-to="translate-y-0 opacity-100"
+            leave="transition transform"
+            leave-from="translate-y-0 opacity-100"
+            leave-to="translate-y-2 opacity-0"
+        >
+            <base-card :class="`absolute top-11 ${origin == 'left' ? 'left-0 right-auto' : 'right-0 left-auto'}`">
+                <slot name="default"></slot>
+            </base-card>
+        </transition-root>
     </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import { TransitionRoot } from "@headlessui/vue";
 
 @Options({
     name: "base-dropdown",
@@ -34,6 +46,9 @@ import { Options, Vue } from "vue-class-component";
             type: String,
         },
     },
+    components: {
+        TransitionRoot,
+    }
 })
 export default class BaseDropdown extends Vue {
     public label = "";
