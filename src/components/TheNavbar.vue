5c8c7c06f8aa967399cce2797fc5b6ea8d40732f
Sigve Hansen <sighan12@gmail.com>
2021-06-21 10:15:45 +0200

    disable nav search input when in search view

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -2,7 +2,7 @@
     <nav class="z-10 bg-white sm:w-16 md:w-full shadow-md md:max-w-xs flex justify-between sm:justify-start sm:flex-col sticky top-0 left-0 dark:bg-secondary">
         <nav-logo />
         <div class="p-4 hidden md:block">
-            <full-search-input />
+            <full-search-input :disabled="$route.name == 'search'" />
         </div>
         <nav-links />
         <playlist-list />
