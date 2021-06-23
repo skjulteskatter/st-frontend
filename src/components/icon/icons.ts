f1176c664fca63d216103a5db152bbe74532d640
Sigve Hansen <sighan12@gmail.com>
2021-06-23 09:23:21 +0200

    add link to all tags

--- src/components/icon/icons.ts
+++ src/components/icon/icons.ts
@@ -127,6 +127,9 @@ const icons: {
     selector: {
         path: "M8 9l4-4 4 4m0 6l-4 4-4-4",
     },
+    tag: {
+        path: "M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z",
+    },
 };
 
 export default icons;
