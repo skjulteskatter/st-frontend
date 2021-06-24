c0d96b4c9d32f1309ceec70bbbb29a0be168e0fd
Sigve Hansen <sighan12@gmail.com>
2021-06-24 15:10:52 +0200

    add language and key selectors

--- src/components/settings/SettingsDropdown.vue
+++ src/components/settings/SettingsDropdown.vue
@@ -6,8 +6,32 @@
 		</template>
 		
 		<div class="flex flex-col gap-2 min-w-max">
-			<icon name="moon" @click="themes.setTheme('dark')" class="dark:hidden cursor-pointer" />
-			<icon name="sun" @click="themes.setTheme('light')" class="hidden dark:block cursor-pointer" />
+			<span class="ml-auto">
+				<icon name="moon" @click="themes.setTheme('dark')" class="dark:hidden cursor-pointer" />
+				<icon name="sun" @click="themes.setTheme('light')" class="hidden dark:block cursor-pointer" />
+			</span>
+			<select
+				v-model="selectedLanguage"
+				@change="setLanguage"
+				class="rounded border-gray-300 text-sm bg-transparent dark:border-gray-500"
+			>
+				<option
+					v-for="lang in languages"
+					:key="lang.key"
+					:value="lang"
+				>{{ lang.name }}</option>
+			</select>
+			<select
+				v-model="selectedKey"
+				@change="setKey"
+				class="rounded border-gray-300 text-sm bg-transparent dark:border-gray-500"
+			>
+				<option
+					v-for="key in transpositions"
+					:key="key"
+					:value="key"
+				>{{ key }}</option>
+			</select>
 			<router-link :to="{ name: 'settings' }">
 				<p class="text-sm rounded bg-black bg-opacity-10 p-2 hover:bg-opacity-20">{{ $t('settings.goToSettings') }}</p>
 			</router-link>
@@ -17,16 +41,75 @@
 
 <script lang="ts">
 import themes, { Themes } from "@/classes/themes";
+import { appSession } from "@/services/session";
 import { useStore } from "@/store";
+import { SessionActionTypes } from "@/store/modules/session/action-types";
+import { SessionMutationTypes } from "@/store/modules/session/mutation-types";
 import { Vue } from "vue-class-component";
 
 export default class SettingsDropdown extends Vue {
 	public store = useStore();
 	public theme = localStorage.getItem("theme") ?? "dark";
     public themes: Themes = themes;
+	public selectedLanguage: Language = {} as Language;
+	public selectedKey = "";
+
+	public transpositions = [
+        "Ab",
+        "A",
+        "Bb",
+        "B",
+        "C",
+        "Db",
+        "D",
+        "Eb",
+        "E",
+        "F",
+        "F#",
+        "G",
+    ];
+
+	public mounted() {
+		this.selectedLanguage =
+            this.languages.find(
+                (l) => l.key == this.user?.settings?.languageKey,
+            ) ??
+            this.languages.find((l) => l.key == "no") ??
+            ({} as Language);
+
+        this.selectedKey = this.user?.settings?.defaultTransposition ?? "C";
+	}
+
+	public async save() {
+        await this.store.dispatch(SessionActionTypes.SESSION_SAVE_SETTINGS);
+	}
+
+	public setLanguage() {
+        const settings = Object.assign({}, this.user?.settings);
+        const language = this.selectedLanguage;
+        if (language) {
+            settings.languageKey = language.key;
+            this.store.commit(SessionMutationTypes.SET_SETTINGS, settings);
+			this.save();
+        }
+    }
+
+    public setKey() {
+        const settings = Object.assign({}, this.user?.settings);
+        const key = this.selectedKey;
+        if (key) {
+            settings.defaultTransposition = key;
+            this.store.commit(SessionMutationTypes.SET_SETTINGS, settings);
+			this.save();
+        }
+    }
 
 	public get user() {
 		return this.store.getters.user;
 	}
+
+	public get languages(): Language[] {
+        return appSession.languages || [];
+    }
 }
 </script>
