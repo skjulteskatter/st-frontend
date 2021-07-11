6ac78f89fd37311b6de28526c2085e7af6244f56
Fredrik Vedvik <fredrik@vedvik.tech>
2021-07-11 14:05:29 +0200

    k

---
+++ src/views/PerformanceView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div v-for="(verse, i) in CurrentVerses" :key="verse.name + i.toString()">
+        <hr />
+        <p v-for="(line, i) in verse.content" :key="i">
+            {{line}}
+        </p>
+    </div>
+</template>
+<script lang="ts">
+import { Lyrics } from "@/classes";
+import { ApiLyrics } from "dmb-api";
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+    name: "performance-view",
+    props: {
+        lyrics: {
+            type: Object,
+        },
+    },
+})
+export default class PerformanceView extends Vue {
+    public currentVerse = 0;
+    public twoVerses = false;
+    private lyrics?: Lyrics;
+
+    public get Lyrics() {
+        return this.lyrics ?? new Lyrics({} as ApiLyrics);
+    }
+
+    public get CurrentVerses() {
+        const verses = Object.values(this.Lyrics.verses);
+
+        const result = [verses[this.currentVerse]];
+
+        if (this.twoVerses) {
+            result.push(verses[this.currentVerse + 1]);
+        }
+
+        return result;
+    }
+}
+</script>
