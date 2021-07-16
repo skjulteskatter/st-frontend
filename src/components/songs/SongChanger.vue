0b492c3bfc123dd33a5c2d86836e7281908cb79f
Sigve Hansen <sighan12@gmail.com>
2021-07-16 14:42:59 +0200

    add song-changer component

---
+++ src/components/songs/SongChanger.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="p-2 flex gap-2 items-center rounded-md border border-gray-300 text-gray-500 dark:border-gray-500 dark:text-gray-300">
+		<icon name="arrowLeft" @click="$emit('previous')" class="cursor-pointer p-1 rounded" />
+		<p v-if="label">{{ label }}</p>
+		<icon name="arrowRight" @click="$emit('next')" class="cursor-pointer p-1 rounded" />
+	</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+	name: "song-changer",
+	props: {
+		label: {
+			type: String,
+		},
+	},
+	emits: ["next", "previous"],
+})
+export default class SongChanger extends Vue {
+	public label?: string;
+}
+</script>
