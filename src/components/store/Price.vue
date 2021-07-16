e8fce7fe8c43408c86d8571616ad3cb96300f3d8
Fredrik Vedvik <fredrik@vedvik.tech>
2021-07-16 14:21:38 +0200

    yearly subscriptions

--- src/components/store/Price.vue
+++ src/components/store/Price.vue
@@ -1,14 +1,15 @@
 <template>
     <div v-if="product">
         {{ product.currency?.toUpperCase() }} 
-        <span v-if="discounted" class="line-through text-gray-400">{{product.originalPrice}}</span>
-        <span v-if="discounted" class="ml-1">{{product.discountedPrice}}</span>
-        <span v-else>{{product.originalPrice}}</span> / {{$t('month').toLowerCase()}}
+        <span v-if="discounted" class="line-through text-gray-400">{{yearly ? product.originalPrice * 12 : product.originalPrice}}</span>
+        <span v-if="discounted" class="ml-1">{{yearly ? product.discountedPrice * 12 : product.discountedPrice}}</span>
+        <span v-else>{{yearly ? product.originalPrice * 12 : product.originalPrice}}</span> / {{$t(yearly ? 'year' : 'month').toLowerCase()}}
     </div>
 </template>
 <script lang="ts">
 import { Product } from "@/classes";
 import http from "@/services/http";
+import { useStore } from "@/store";
 import { Options, Vue } from "vue-class-component";
 
 @Options({
@@ -20,8 +21,13 @@ import { Options, Vue } from "vue-class-component";
     },
 })
 export default class Price extends Vue {
+    private store = useStore();
     public product?: Product;
     public country = "no";
+
+    public get yearly() {
+        return this.store.state.stripe.type == "year";
+    }
 
     public async mounted() {
         this.country = await http.getCountry();
