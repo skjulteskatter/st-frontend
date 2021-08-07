7ad15580bafeb15dc9bee6fc64fd5b9e8353d618
Sigve Hansen <sighan12@gmail.com>
2021-08-07 17:02:56 +0200

    loading states

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -7,7 +7,7 @@
                 <base-button
                     :class="{ disabled: disableButton }"
                     @click="refreshUsers"
-                    :loading="loading"
+                    :loading="loading['refresh']"
                     :content="false"
                     icon="refresh"
                     theme="primary"
@@ -17,45 +17,47 @@
             </div>
         </div>
         <base-card class="overflow-x-auto">
-            <table class="min-w-full" v-if="Users.length">
-                <thead>
-                    <tr>
-                        <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">{{ $t("common.name") }}</th>
-                        <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">Email</th>
-                        <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">{{ $t("common.role") }}</th>
-                        <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">Last Login</th>
-                        <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">Id</th>
-                        <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider"><span class="sr-only">{{$t('common.edit')}}</span></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="u in Users" :key="u.id">
-                        <td class="flex gap-4 items-center px-4 py-3 whitespace-nowrap">
-                            <img
-                                :src="
-                                    u.image ?? '/img/portrait-placeholder.png'
-                                "
-                                class="w-8 h-8 object-cover rounded-full"
-                            />
-                            <span>{{ u.displayName }}</span>
-                        </td>
-                        <td class="px-4 py-3 whitespace-nowrap">{{ u.email }}</td>
-                        <td class="px-4 py-3 whitespace-nowrap">
-                            <span v-if="u.roles[0]" :class="[u.roles[0] == 'administrator' ? 'bg-green-500/20 text-green-600 dark:bg-green-200/20 dark:text-green-200' : 'bg-yellow-500/20 text-yellow-600 dark:bg-yellow-200/20 dark:text-yellow-300', 'rounded-full text-xs tracking-wide py-1 px-2']">
-                                {{ u.roles[0] }}
-                            </span>
-                        </td>
-                        <td class="px-4 py-3 whitespace-nowrap">
-                            {{ new Date(u.lastLogin).toLocaleString() }}
-                        </td>
-                        <td class="px-4 py-3 whitespace-nowrap">{{ u.id }}</td>
-                        <td class="px-4 py-3 whitespace-nowrap">
-                            <edit-user v-if="u.id != User?.id" :user="u" @save="saveRoles(u)" />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <p v-else class="w-full text-center text-gray-500">No users to show</p>
+            <loader :loading="loading['search']" position="local">
+                <table class="min-w-full" v-if="Users.length">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">{{ $t("common.name") }}</th>
+                            <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">Email</th>
+                            <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">{{ $t("common.role") }}</th>
+                            <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">Last Login</th>
+                            <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider">Id</th>
+                            <th scope="col" class="px-4 py-3 text-left text-xs font-medium opacity-50 uppercase tracking-wider"><span class="sr-only">{{$t('common.edit')}}</span></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="u in Users" :key="u.id">
+                            <td class="flex gap-4 items-center px-4 py-3 whitespace-nowrap">
+                                <img
+                                    :src="
+                                        u.image ?? '/img/portrait-placeholder.png'
+                                    "
+                                    class="w-8 h-8 object-cover rounded-full"
+                                />
+                                <span>{{ u.displayName }}</span>
+                            </td>
+                            <td class="px-4 py-3 whitespace-nowrap">{{ u.email }}</td>
+                            <td class="px-4 py-3 whitespace-nowrap">
+                                <span v-if="u.roles[0]" :class="[u.roles[0] == 'administrator' ? 'bg-green-500/20 text-green-600 dark:bg-green-200/20 dark:text-green-200' : 'bg-yellow-500/20 text-yellow-600 dark:bg-yellow-200/20 dark:text-yellow-300', 'rounded-full text-xs tracking-wide py-1 px-2']">
+                                    {{ u.roles[0] }}
+                                </span>
+                            </td>
+                            <td class="px-4 py-3 whitespace-nowrap">
+                                {{ new Date(u.lastLogin).toLocaleString() }}
+                            </td>
+                            <td class="px-4 py-3 whitespace-nowrap">{{ u.id }}</td>
+                            <td class="px-4 py-3 whitespace-nowrap">
+                                <edit-user v-if="u.id != User?.id" :user="u" @save="saveRoles(u)" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p v-else class="w-full text-center text-gray-500">No users to show</p>
+            </loader>
         </base-card>
     </div>
 </template>
@@ -92,7 +94,9 @@ export default class UsersList extends Vue {
     public users?: User[];
     public currentUser?: User;
     public disableButton = false;
-    public loading = false;
+    public loading: {
+        [key: string]: boolean;
+    } = {};
 
     public userQuery = "";
 
@@ -102,18 +106,20 @@ export default class UsersList extends Vue {
     }
 
     public async searchUser() {
-        
-        if (this.userQuery)
+        this.loading["search"] = true;
+        if (this.userQuery) {
             this.store.commit(UsersMutationTypes.SET_USERS, await api.admin.getUsers(this.userQuery));
+        }
+        this.loading["search"] = false;
     }
 
     public async refreshUsers() {
-        this.loading = true;
+        this.loading["refresh"] = true;
         this.disableButton = true;
         await this.searchUser();
 
         notify("success", this.$t("notification.fetchedusers"), "check");
-        this.loading = false;
+        this.loading["refresh"] = false;
         this.disableButton = false;
     }
 
@@ -130,10 +136,10 @@ export default class UsersList extends Vue {
     }
 
     public async saveRoles(user: User) {
-        this.loading = true;
+        this.loading["save"] = true;
         await this.store.dispatch(UsersActionTypes.SET_ROLES, user);
         notify("success", this.$t("notification.saved"), "check");
-        this.loading = false;
+        this.loading["save"] = false;
     }
 }
 </script>
