0598685dba37828eb496ef2946a0a68fbbab23ed
Fredrik Vedvik <fredrik@vedvik.tech>
2021-08-08 14:12:55 +0200

    adminservice

--- src/components/admin/EditUser.vue
+++ src/components/admin/EditUser.vue
@@ -1,77 +1,80 @@
 <template>
 	<base-button theme="tertiary" class="px-2 py-1" @click="showModal()">{{ $t('common.edit') }}</base-button>
-	<base-modal v-if="user" :show="show" @close="hideModal()">
-		<loader :loading="loading" />
-		<div class="flex flex-col gap-4">
-			<div class="flex gap-4 items-center">
-				<img
-					:src="
-						user.image ??
-						'/img/portrait-placeholder.png'
-					"
-					class="rounded-full w-20 h-20 object-cover"
-				/>
-				<span class="flex flex-col">
-					<h3 class="font-bold">
-						{{ user.displayName }}
-					</h3>
-					<small class="text-gray-500">{{ user.email }}</small>
-				</span>
-			</div>
-			<div class="flex flex-col">
-				<div class="flex flex-col">
-					<label
-						class="font-bold"
-						>Roles</label
-					>
-					<label
-						class="text-sm"
-						v-for="role in roles"
-						:key="role"
-					>
-						<input
-							type="checkbox"
-							class="text-primary rounded border-gray-300 focus:ring-primary"
-							:checked="
-								user.roles.find(
-									(r) => r == role
-								)
-							"
-							@change="toggleRole(user, role)"
-						/>
-						{{ role }}
-					</label>
+	<base-modal v-if="User" :show="show" @close="hideModal()">
+		<loader :loading="loading">
+			<div class="flex flex-col gap-4">
+				<div class="flex gap-4 items-center">
+					<img
+						:src="
+							User.image ??
+							'/img/portrait-placeholder.png'
+						"
+						class="rounded-full w-20 h-20 object-cover"
+					/>
+					<span class="flex flex-col">
+						<h3 class="font-bold">
+							{{ User.displayName }}
+						</h3>
+						<small class="text-gray-500">{{ User.email }}</small>
+					</span>
 				</div>
-				<br/>
 				<div class="flex flex-col">
-					<h3 class="font-bold">{{$t('common.collections')}}</h3>
-					<div v-for="i in user.subscriptions" :key="i.id">
-						<span>{{getCollection(i)?.getName()}}</span>
-						<icon class="cursor-pointer" @click="deleteSub(user, i)" size="24" name="trash" v-if="i.id.startsWith('custom')" />
+					<div class="flex flex-col">
+						<label
+							class="font-bold"
+							>Roles</label
+						>
+						<label
+							class="text-sm"
+							v-for="role in roles"
+							:key="role"
+						>
+							<input
+								type="checkbox"
+								class="text-primary rounded border-gray-300 focus:ring-primary"
+								:checked="
+									User.roles.find(
+										(r) => r == role
+									)
+								"
+								@change="toggleRole(User, role)"
+							/>
+							{{ role }}
+						</label>
 					</div>
-					<base-dropdown
-						:label="'Add'"
-					>
-						<div class="mb-2 text-sm" v-for="i in getUnownedCollections(user.subscriptions)" :key="i.id">
-							<label :key="i.id" class="cursor-pointer">
-								<input :key="i.id" type="checkbox" v-model="newSubs[user.id][i.id]" />
-								{{i.getName()}}
-							</label>
+					<br/>
+					<div class="flex flex-col">
+						<h3 class="font-bold">{{$t('common.collections')}}</h3>
+						<div v-for="i in User.subscriptions" :key="i.id" class="mb-2">
+							<span class="text-sm">{{i.id}}</span><icon class="cursor-pointer" @click="deleteSub(User, i)" size="24" name="trash" v-if="i.id.startsWith('custom')" />
+							<div v-for="c in getCollections(i)" :key="c.id">{{c.getName()}}</div>
+							<hr />
+							<br/>
 						</div>
-						<br/>
-						<span class="text-sm">Valid To</span>
-						<input type="date" v-model="validTo[user.id]"/>
-						<base-button class="mb-2" :disabled="!Object.values(newSubs[user.id]).includes(true) || !validTo[user.id]" @click="addSubscriptions(user)">Save</base-button>
-					</base-dropdown>
+						<base-dropdown
+							:label="'Add'"
+						>
+							<div class="mb-2 text-sm" v-for="i in getUnownedCollections(User.subscriptions)" :key="i.id">
+								<label :key="i.id" class="cursor-pointer">
+									<input :key="i.id" type="checkbox" v-model="newSubs[User.id][i.id]" />
+									{{i.getName()}}
+								</label>
+							</div>
+							<br/>
+							<span class="text-sm">Valid To</span>
+							<input type="date" v-model="validTo[User.id]"/>
+							<base-button class="mb-2" :disabled="!Object.values(newSubs[User.id]).includes(true) || !validTo[User.id]" @click="addSubscriptions(User)">Save</base-button>
+						</base-dropdown>
+					</div>
 				</div>
+				<base-button
+					theme="secondary"
+					@click="$emit('save')"
+					:loading="loading"
+					>{{ $t("common.save") }}</base-button
+				>
 			</div>
-			<base-button
-				theme="secondary"
-				@click="$emit('save')"
-				:loading="loading"
-				>{{ $t("common.save") }}</base-button
-			>
-		</div>
+		</loader>
 	</base-modal>
 </template>
 
@@ -80,7 +83,8 @@ import { Options, Vue } from "vue-class-component";
 import { BaseModal } from "@/components";
 import { useStore } from "@/store";
 import { UsersMutationTypes } from "@/store/modules/users/mutation-types";
-import api from "@/services/api";
+import { adminService } from "@/services/admin";
+import api, { admin } from "@/services/api";
 
 @Options({
 	name: "edit-user",
@@ -99,25 +103,29 @@ export default class EditUser extends Vue {
 	public store = useStore();
 	public user?: User;
 
+	public get User() {
+		return this.user as User;
+	}
+
     public loading = false;
 	public show = false;
-
-	public newSubs: {
-        [key: string]: {
-            [key: string]: boolean;
-        };
-    } = {};
-
-    public validTo: {
-        [key: string]: string;
-    } = {};
 
     public get roles() {
         return this.store.state.users.roles;
     }
 
-	public showModal() {
+	public newSubs = adminService.NewSubs;
+
+	public validTo = adminService.ValidTo;
+
+	public async showModal() {
 		this.show = true;
+		this.loading = true;
+		const id = this.User.id;
+		this.User.subscriptions = (await admin.getUser(id)).subscriptions;
+		this.newSubs[id] = {};
+		this.validTo[id] = "";
+		this.loading = false;
 	}
 
 	public hideModal() {
@@ -128,8 +136,8 @@ export default class EditUser extends Vue {
         this.store.commit(UsersMutationTypes.USER_TOGGLE_ROLE, { user, role });
     }
 
-    public getCollection(subscription: Subscription) {
-        return this.store.getters.collections.find(i => subscription.collectionIds.includes(i.id));
+    public getCollections(subscription: Subscription) {
+        return this.store.getters.collections.filter(i => subscription.collectionIds.includes(i.id));
     }
 
     public getUnownedCollections(subscriptions: Subscription[]) {
---
+++ src/services/admin.ts
@@ -0,0 +1,44 @@
+import { useStore } from "@/store";
+import api from "./api";
+
+export class AdminService {
+    private store = useStore();
+    private initialized = false;
+
+    private users: User[] = [];
+    public get Users() {
+        return this.users;
+    }
+
+    private roles: string[] = [];
+    public get Roles() {
+        return this.roles;
+    }
+
+    public async initialize() {
+        if (this.initialized == false)
+            return;
+
+        this.roles = await api.admin.getRoles();
+    }
+
+    public async getUsers(query: string) {
+        this.users = await api.admin.getUsers(query);
+    }
+
+    public async getUser(id: string) {
+        return await api.admin.getUser(id);
+    }
+
+	public NewSubs: {
+        [key: string]: {
+            [key: string]: boolean;
+        };
+    } = {};
+
+    public ValidTo: {
+        [key: string]: string;
+    } = {};
+}
+
+export const adminService = new AdminService();
