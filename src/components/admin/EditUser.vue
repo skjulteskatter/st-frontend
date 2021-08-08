6781020d7051297b6067b6571295aab297183210
Sigve Hansen <sighan12@gmail.com>
2021-08-08 18:42:43 +0200

    replace modal with slide panel

--- src/components/admin/EditUser.vue
+++ src/components/admin/EditUser.vue
@@ -1,7 +1,7 @@
 <template>
 	<base-button theme="tertiary" class="px-2 py-1" @click="showModal()">{{ $t('common.edit') }}</base-button>
-	<base-modal v-if="User" :show="show" @close="hideModal()">
-		<loader :loading="loading">
+	<slide-panel :open="show" @close="hideModal()" :title="$t('common.edit')">
+		<loader :loading="loading" position="local">
 			<div class="flex flex-col gap-4">
 				<div class="flex gap-4 items-center">
 					<img
@@ -18,20 +18,17 @@
 						<small class="text-gray-500">{{ User.email }}</small>
 					</span>
 				</div>
-				<div class="flex flex-col">
-					<div class="flex flex-col">
+				<div class="flex flex-col divide-y divide-gray-200">
+					<div class="flex flex-col gap-1 py-4">
+						<small class="mb-2 opacity-50 text-xs font-medium uppercase tracking-wider">{{$t('common.roles')}}</small>
 						<label
-							class="font-bold"
-							>Roles</label
-						>
-						<label
-							class="text-sm"
+
 							v-for="role in roles"
 							:key="role"
 						>
 							<input
 								type="checkbox"
-								class="text-primary rounded border-gray-300 focus:ring-primary"
+								class="text-primary rounded border-gray-300 focus:ring-primary mr-1"
 								:checked="
 									User.roles.find(
 										(r) => r == role
@@ -42,21 +39,18 @@
 							{{ role }}
 						</label>
 					</div>
-					<br/>
-					<div class="flex flex-col">
-						<h3 class="font-bold">{{$t('common.collections')}}</h3>
+					<div class="flex flex-col py-4">
+						<small class="mb-2 opacity-50 text-xs font-medium uppercase tracking-wider">{{$t('common.collections')}}</small>
 						<div v-for="i in User.subscriptions" :key="i.id" class="mb-2">
 							<span class="text-sm">{{i.id}}</span><icon class="cursor-pointer" @click="deleteSub(User, i)" size="24" name="trash" v-if="i.id.startsWith('custom')" />
 							<div v-for="c in getCollections(i)" :key="c.id">{{c.getName()}}</div>
-							<hr />
-							<br/>
 						</div>
 						<base-dropdown
 							:label="'Add'"
 						>
 							<div class="mb-2 text-sm" v-for="i in getUnownedCollections(User.subscriptions)" :key="i.id">
 								<label :key="i.id" class="cursor-pointer">
-									<input :key="i.id" type="checkbox" v-model="newSubs[User.id][i.id]" />
+									<input :key="i.id" type="checkbox" class="text-primary rounded border-gray-300 focus:ring-primary mr-1" v-model="newSubs[User.id][i.id]" />
 									{{i.getName()}}
 								</label>
 							</div>
@@ -75,12 +69,12 @@
 				>
 			</div>
 		</loader>
-	</base-modal>
+	</slide-panel>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { BaseModal } from "@/components";
+import SlidePanel from "@/components/SlidePanel.vue";
 import { useStore } from "@/store";
 import { UsersMutationTypes } from "@/store/modules/users/mutation-types";
 import { adminService } from "@/services/admin";
@@ -89,7 +83,7 @@ import api, { admin } from "@/services/api";
 @Options({
 	name: "edit-user",
 	components: {
-		BaseModal,
+		SlidePanel,
 	},
 	props: {
 		user: {
