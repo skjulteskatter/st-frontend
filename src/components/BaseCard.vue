c6430e9353321ae0277dec9ab6898e2a6b2790d1
Sigve Hansen <sighan12@gmail.com>
2021-08-11 15:28:50 +0200

    remove hidden overflow

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="bg-white rounded-lg shadow-md dark:bg-secondary overflow-hidden">
+    <div class="bg-white rounded-lg shadow-md dark:bg-secondary">
         <div class="p-4 border-b border-gray-300 dark:border-gray-500" v-if="$slots.header">
             <slot name="header" />
         </div>
         <div class="p-4 h-full">
             <slot name="default" />
         </div>
-        <div class="p-4 bg-gray-100 dark:bg-black/20" v-if="$slots.footer">
+        <div class="p-4 border-t border-gray-300 dark:border-gray-500" v-if="$slots.footer">
             <slot name="footer" />
         </div>
     </div>
