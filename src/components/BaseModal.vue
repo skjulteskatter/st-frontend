9c38550e93a5beb34c8029b08974a4d9df05c39b
Sigve Hansen <sighan12@gmail.com>
2021-08-12 21:15:13 +0200

    animation

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -14,10 +14,10 @@
         <transition-child
             as="div"
             class="max-w-xl"
-            enter="transition transform"
-            enter-from="translate-y-4"
-            enter-to="translate-y-0"
-            leave="transition transform"
+            enter="transition"
+            enter-from="scale-90"
+            enter-to="scale-100"
+            leave="transition"
             leave-from="translate-y-0"
             leave-to="translate-y-4"
         >
