9815ec6d4dde95e2c314558fba44b3c521048695
Sigve Hansen <sighan12@gmail.com>
2021-08-13 10:42:46 +0200

    make card button

--- src/components/playlist/PlaylistCard.vue
+++ src/components/playlist/PlaylistCard.vue
@@ -1,23 +1,23 @@
 <template>
-    <base-card
-        class="cursor-pointer hover:ring-2 hover:ring-gray-400"
-        :class="{ 'border border-primary': userId != playlist?.userId }"
-    >
-        <button class="text-left w-full rounded focus:outline-none focus:ring focus:ring-primary ring-offset-2" @click="goToPlaylist">
-            <FolderIcon class="w-8 h-8 opacity-50 mb-2" />
-            <strong class="font-bold md:max-w-md md:overflow-x-hidden md:overflow-ellipsis md:whitespace-nowrap">{{ playlist?.name }}</strong>
-            <small class="opacity-50 block">
-                {{ playlist?.entries.length || $t('common.no-amount') }}
-                {{ $t("common.songs").toLowerCase() }}
-            </small>
-        </button>
-        <template #footer v-if="playlist?.sharedWithIds.length">
-            <small class="opacity-50 ml-auto flex gap-1 items-center">
-                <ShareIcon class="w-4 h-4" />
-                {{ `${$t('playlist.sharedWith')} ${playlist?.sharedWithIds.length}` }}
-            </small>
-        </template>
-    </base-card>
+    <button class="text-left rounded-lg focus:outline-none focus:ring-2 focus:ring-primary ring-offset-2" @click="goToPlaylist">
+        <base-card
+            class="h-full cursor-pointer hover:ring-2 hover:ring-gray-400"
+            :class="{ 'border border-primary': userId != playlist?.userId }"
+        >
+                <FolderIcon class="w-8 h-8 opacity-50 mb-2" />
+                <strong class="font-bold md:max-w-md md:overflow-x-hidden md:overflow-ellipsis md:whitespace-nowrap">{{ playlist?.name }}</strong>
+                <small class="opacity-50 block">
+                    {{ playlist?.entries.length || $t('common.no-amount') }}
+                    {{ $t("common.songs").toLowerCase() }}
+                </small>
+            <template #footer v-if="playlist?.sharedWithIds.length">
+                <small class="opacity-50 ml-auto flex gap-1 items-center">
+                    <ShareIcon class="w-4 h-4" />
+                    {{ `${$t('playlist.sharedWith')} ${playlist?.sharedWithIds.length}` }}
+                </small>
+            </template>
+        </base-card>
+    </button>
 </template>
 
 <script lang="ts">
