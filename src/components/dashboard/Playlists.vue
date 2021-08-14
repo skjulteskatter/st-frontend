5f034055c5e3170ae61c358f3a7693b29faf4293
Sigve Hansen <sighan12@gmail.com>
2021-08-14 23:50:58 +0200

    replace portrait placeholder + letter spacing

--- src/components/dashboard/Playlists.vue
+++ src/components/dashboard/Playlists.vue
@@ -26,7 +26,7 @@
                     <strong>
                         {{ p.name }}
                     </strong>
-                    <small class="text-gray-400 block tracking-wide">
+                    <small class="text-gray-400 block tracking-wider">
                         {{ p.entries.length || $t('common.no-amount') }}
                         {{ $t("common.songs").toLowerCase() }}
                     </small>
