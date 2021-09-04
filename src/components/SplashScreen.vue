c8fa66e156003292f2f8dc921f921324fe10bb46
Sigve Hansen <sighan12@gmail.com>
2021-09-05 00:17:02 +0200

    add logo

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -92,6 +92,7 @@
 					</div>
 				</div>
 			</div>
+			<img class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-pulse w-20 h-20 drop-shadow-lg" src="/img/logo/icon.svg" alt="SongTreasures logo">
 		</section>
 	</transition-root>
 </template>
