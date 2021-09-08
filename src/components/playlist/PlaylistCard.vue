713ecd688c76b5fdeeeba3eb0498cf130e787d0f
Sigve Hansen <sighan12@gmail.com>
2021-09-08 09:43:46 +0200

    improve playlist card

--- src/components/playlist/PlaylistCard.vue
+++ src/components/playlist/PlaylistCard.vue
@@ -1,18 +1,19 @@
 <template>
     <button class="text-left rounded-lg focus:outline-none focus:ring-2 focus:ring-primary ring-offset-2" @click="goToPlaylist">
-        <base-card
-            class="h-full cursor-pointer hover:ring-2 hover:ring-gray-400"
-            :class="{ 'border border-primary': userId != playlist?.userId }"
-        >
-                <FolderIcon class="w-8 h-8 opacity-50 mb-2" />
-                <strong class="font-bold md:max-w-md md:overflow-x-hidden md:overflow-ellipsis md:whitespace-nowrap">{{ playlist?.name }}</strong>
-                <small class="opacity-50 block">
-                    {{ playlist?.entries.length || $t('common.no-amount') }}
-                    {{ $t("common.songs").toLowerCase() }}
-                </small>
+        <base-card class="h-full cursor-pointer hover:ring-2 hover:ring-gray-400">
+            <div class="flex gap-4">
+                <FolderIcon class="w-6 h-6 opacity-50" />
+                <span>
+                    <strong class="font-bold md:max-w-md md:overflow-x-hidden md:overflow-ellipsis md:whitespace-nowrap">{{ playlist?.name }}</strong>
+                    <small class="opacity-50 block">
+                        {{ playlist?.entries.length || $t('common.no-amount') }}
+                        {{ $t("common.songs").toLowerCase() }}
+                    </small>
+                </span>
+            </div>
             <template #footer v-if="playlist?.sharedWithIds.length">
                 <small class="ml-auto w-max flex gap-2 items-center" :class="[userId != playlist?.userId ? 'text-primary' : 'opacity-50']">
-                    <ShareIcon class="w-4 h-4" />
+                    <ShareIcon class="w-3 h-3" />
                     <span v-if="userId != playlist?.userId">{{ $t('playlist.sharedWithYou') }}</span>
                     <span v-else>{{ `${$t('playlist.sharedWith')} ${playlist?.sharedWithIds.length}` }}</span>
                 </small>
