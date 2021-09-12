f0652115e79c6c722d26f6161be6a0abfa7c3c26
Fredrik Vedvik <fredrik@vedvik.tech>
2021-09-12 12:47:01 +0200

    sss

--- src/components/playlist/PlaylistSongCard.vue
+++ src/components/playlist/PlaylistSongCard.vue
@@ -1,43 +1,38 @@
 <template>
-    <base-card v-if="collection && song && playlist" class="hover:ring-2 hover:ring-gray-400">
+    <base-card @click="goToSong" class="hover:ring-2 hover:ring-gray-400"
+        :class="{'cursor-pointer': !disabled}"
+        :style="disabled ? 'opacity: 0.5' : ''"   
+    >
         <div class="flex gap-4 items-center">
             <SelectorIcon class="w-5 h-5 opacity-50 cursor-move" />
-            <router-link
-                v-if="!disabled"
+            <div
                 class="flex-grow rounded focus:outline-none focus:ring focus:ring-primary ring-offset-2"
-                :to="{
-                    name: 'song',
-                    params: {
-                        collection: collection.key,
-                        number: song.number,
-                    },
-                }"
             >
                 <div class="flex flex-col">
                     <span class="font-semibold">
-                        {{ entryName }}
+                        {{ song?.getName() }}
                     </span>
-                    <small class="text-gray-500 dark:text-gray-400">
-                        {{ collection.getName(languageKey) }}
-                        {{ song.number }}
-                    </small>
-                </div>
-            </router-link>
-            <div v-else>
-                <div class="flex flex-col" style="opacity: 0.5">
-                    <span class="font-semibold">
-                        {{ entryName }}
-                    </span>
-                    <small class="text-gray-500 dark:text-gray-400">
-                        {{ collection.getName(languageKey) }}
-                        {{ song.number }}
+                    <small v-for="collection in Collections" :key="collection.id" class="text-gray-500 dark:text-gray-400">
+                        {{ collection.getName() }}
+                        {{ song?.getNumber(collection.id) }}
                     </small>
                 </div>
             </div>
+            <!-- <div v-else>
+                <div class="flex flex-col" style="opacity: 0.5">
+                    <span class="font-semibold">    
+                        {{ song?.getName() }}
+                    </span>
+                    <small v-for="collection in Collections" :key="collection.id" class="text-gray-500 dark:text-gray-400">
+                        {{ collection.getName() }}
+                        {{ song?.getNumber(collection.id) }}
+                    </small>
+                </div>
+            </div> -->
             <button
                 class="text-xs tracking-wide text-red-500 px-2 py-1 rounded-md hover:bg-red-500/10 cursor-pointer dark:text-red-400"
-                v-if="playlist.userId == userId"
-                @click="removeFromPlaylist"
+                v-if="canEdit"
+                @click="remove"
             >
                 {{ $t("playlist.remove") }}
             </button>
@@ -47,12 +42,10 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { ApiPlaylist, ApiPlaylistEntry } from "dmb-api";
+import { ApiPlaylistEntry } from "dmb-api";
 import { Song } from "@/classes";
 import { useStore } from "@/store";
-import { SessionActionTypes } from "@/store/modules/session/action-types";
 import { appSession } from "@/services/session";
-import { notify } from "@/services/notify";
 import { songs } from "@/services/api";
 import { SelectorIcon } from "@heroicons/vue/solid";
 
@@ -63,54 +56,57 @@ import { SelectorIcon } from "@heroicons/vue/solid";
             type: Object,
             required: true,
         },
-        playlist: {
-            type: Object,
-            required: true,
+        canEdit: {
+            type: Boolean,
         },
     },
     components: {
         SelectorIcon,
     },
+    emits: [
+        "remove",
+    ],
 })
 export default class PlaylistSongCard extends Vue {
     private store = useStore();
     public entry?: ApiPlaylistEntry;
-    public playlist?: ApiPlaylist;
     public song: Song | null = null;
-    public disabled = false;
+    public canEdit?: boolean;
+
+    public get disabled() {
+        return this.song?.available !== true;
+    }
+
+    public get Collections() {
+        return this.song?.Collections ?? [];
+    }
 
     public async mounted() {
         this.song = appSession.songs.find(s => s.id == this.entry?.songId) ?? null;
 
         if (!this.song && this.entry) {
             this.song = new Song(await songs.getSongById(this.entry?.songId));
-            this.disabled = !this.song.available;
         }
     }
 
-    public async removeFromPlaylist() {
-        const title = this.$t("playlist.removed");
-        const content = this.$t("notification.removedsong");
+    public goToSong() {
+        if (this.disabled)
+            return;
+        const collection = this.Collections[0];
 
-        if (!this.playlist?.id || !this.entry?.id) return;
-        await this.store.dispatch(SessionActionTypes.PLAYLIST_REMOVE_ENTRY, {
-            playlistId: this.playlist.id,
-            entryId: this.entry.id,
-        });
-
-        notify("success", title, "trash", content);
+        if (collection) {
+            this.$router.push({
+                name: "song",
+                params: {
+                    collection: collection.key,
+                    number: this.song?.getNumber(collection.id),
+                },
+            });
+        }
     }
 
-    public get languageKey() {
-        return this.store.getters.languageKey;
-    }
-
-    public get collection() {
-        return appSession.collections.find(c => this.song?.collectionIds.includes(c.id));
-    }
-
-    public get entryName() {
-        return this.song ? new Song(this.song).getName(this.languageKey) : "";
+    public remove() {
+        this.$emit("remove", this.entry?.id);
     }
 
     public get userId() {
