2553d68f6daab15784c219d9a9630754eb2a084d
Sigve Hansen <sighan12@gmail.com>
2021-09-13 16:39:13 +0200

    fix lyrics columns

--- src/components/lyrics/LyricsViewer.vue
+++ src/components/lyrics/LyricsViewer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="lyrics-content grid grid-flow-col md:grid-cols-2">
+    <div class="lyrics-content">
         <div
             class="flex-col w-full whitespace-pre-line mb-4"
             v-for="(verse, i) in text"
@@ -42,7 +42,11 @@ export default class LyricsViewer extends Vue {
 </script>
 <style lang="scss">
 .lyrics-content {
-	column-count: 2;
+	column-count: 1;
+
+    @media screen and (min-width: 600px) {
+        column-count: 2;
+    }
 }
 
 .lyrics-content > div {
