ca6e83fca821a15e197628b495675fa70bd519a2
Sigve Hansen <sighan12@gmail.com>
2021-09-24 09:51:37 +0200

    add transitions

--- src/components/playlist/PlaylistCard.vue
+++ src/components/playlist/PlaylistCard.vue
@@ -1,25 +1,32 @@
 <template>
-    <button class="text-left rounded-lg focus:outline-none focus:ring-2 focus:ring-primary ring-offset-2" @click="goToPlaylist">
-        <base-card class="h-full cursor-pointer hover:ring-2 hover:ring-gray-400">
-            <div class="flex gap-4">
-                <FolderIcon class="w-6 h-6 opacity-50" />
-                <span>
-                    <strong class="font-bold md:max-w-md md:overflow-x-hidden md:overflow-ellipsis md:whitespace-nowrap">{{ playlist?.name }}</strong>
-                    <small class="opacity-50 block">
-                        {{ playlist?.entries.length || $t('common_noAmount') }}
-                        {{ $t("common_songs").toLowerCase() }}
+    <transition
+        appear
+        enter-active-class="transition duration-200 ease-out"
+        enter-from-class="opacity-0 translate-y-2"
+        enter-to-class="opacity-100 translate-y-0"
+    >
+        <button class="text-left rounded-lg focus:outline-none focus:ring-2 focus:ring-primary ring-offset-2" @click="goToPlaylist">
+            <base-card class="h-full cursor-pointer hover:ring-2 hover:ring-gray-400">
+                <div class="flex gap-4">
+                    <FolderIcon class="w-6 h-6 opacity-50" />
+                    <span>
+                        <strong class="font-bold md:max-w-md md:overflow-x-hidden md:overflow-ellipsis md:whitespace-nowrap">{{ playlist?.name }}</strong>
+                        <small class="opacity-50 block">
+                            {{ playlist?.entries.length || $t('common_noAmount') }}
+                            {{ $t("common_songs").toLowerCase() }}
+                        </small>
+                    </span>
+                </div>
+                <template #footer v-if="playlist?.sharedWithIds.length">
+                    <small class="ml-auto w-max flex gap-2 items-center" :class="[userId != playlist?.userId ? 'text-primary' : 'opacity-50']">
+                        <ShareIcon class="w-3 h-3" />
+                        <span v-if="userId != playlist?.userId">{{ $t('playlist_sharedWithYou') }}</span>
+                        <span v-else>{{ `${$t('playlist_sharedWith')} ${playlist?.sharedWithIds.length}` }}</span>
                     </small>
-                </span>
-            </div>
-            <template #footer v-if="playlist?.sharedWithIds.length">
-                <small class="ml-auto w-max flex gap-2 items-center" :class="[userId != playlist?.userId ? 'text-primary' : 'opacity-50']">
-                    <ShareIcon class="w-3 h-3" />
-                    <span v-if="userId != playlist?.userId">{{ $t('playlist_sharedWithYou') }}</span>
-                    <span v-else>{{ `${$t('playlist_sharedWith')} ${playlist?.sharedWithIds.length}` }}</span>
-                </small>
-            </template>
-        </base-card>
-    </button>
+                </template>
+            </base-card>
+        </button>
+    </transition>
 </template>
 
 <script lang="ts">
