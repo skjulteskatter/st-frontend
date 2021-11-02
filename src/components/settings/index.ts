f0da38f575c411bfa1c06bd5152cc1630227d512
Sigve Hansen <sighan12@gmail.com>
2021-11-02 09:08:59 +0100

    remove unused component

--- src/components/settings/index.ts
+++ src/components/settings/index.ts
@@ -1,9 +1,7 @@
 import UserCard from "./UserCard.vue";
 import ChangePassword from "./ChangePassword.vue";
-import SettingsDropdown from "./SettingsDropdown.vue";
 
 export {
     UserCard,
     ChangePassword,
-    SettingsDropdown,
 };
