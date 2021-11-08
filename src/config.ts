ec5abd34da013787394eb542e3497e8be60f39d5
Fredrik V <fredrik@vedvik.tech>
2021-11-08 22:25:45 +0100

    remove unused comment

--- src/config.ts
+++ src/config.ts
@@ -16,15 +16,6 @@ export const firebaseConfig = {
     measurementId: "G-YWMEW6TKB8",
 };
 
-// export const firebaseConfig = {
-//     apiKey: "AIzaSyC6b3XqiSM6uBuDq23THGzIWj6OgIogB_U",
-//     authDomain: "songtreasure.app",
-//     projectId: "landax-music-app",
-//     messagingSenderId: "230724783358",
-//     appId: "1:230724783358:web:2642da04c8647c75e6a9af",
-//     measurementId: "G-PP8F4ZZ3M0",
-// };
-
 export default {
     api: {
         basePath: basePath,
