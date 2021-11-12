d478b4ad17f83f7c76efd91d61368dd7cd9bf24f
Fredrik Vedvik <fredrik@landax.no>
2021-11-12 12:58:42 +0100

    caos svg...

---
+++ src/views/index.ts
@@ -0,0 +1,13 @@
+import CollectionView from "./CollectionView.vue";
+import ContributorView from "./ContributorView.vue";
+import CreditSongView from "./CreditSong.vue";
+import DashboardView from "./Dashboard.vue";
+import SearchView from "./dashboard/CompleteSearch.vue";
+
+export {
+    CollectionView,
+    ContributorView,
+    CreditSongView,
+    DashboardView,
+    SearchView,
+};
