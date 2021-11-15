aacc00a77cbed20d82fffd2637637e35faac9dcf
Fredrik Vedvik <fredrik@landax.no>
2021-11-15 09:46:39 +0100

    fixed loading

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,21 +3,15 @@ import BaseButton from "./BaseButton.vue";
 import Modal from "./Modal.vue";
 import BaseModal from "./BaseModal.vue";
 import Loader from "./Loader.vue";
-
 import Copyright from "./Copyright.vue";
-
 import LyricsCard from "./LyricsCard.vue";
 import LyricsSettings from "./LyricsSettings.vue";
-
-export * from "./presentation";
-export * from "./store";
-export * from "./admin";
-
 import SettingsCard from "./SettingsCard.vue";
 import CollectionCard from "./CollectionCard.vue";
 import BackButton from "./BackButton.vue";
-
 import Tooltip from "./Tooltip.vue";
+
+export * from "./store";
 
 export {
     BaseCard,
