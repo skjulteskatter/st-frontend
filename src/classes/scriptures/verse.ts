2275b640ee2bea027f5f86b6ab1d56c6e4abed78
Sigve Hansen <sighan12@gmail.com>
2021-11-16 10:03:30 +0100

    remove bibleReferences

--- src/classes/scriptures/verse.ts
+++ src/classes/scriptures/verse.ts
@@ -4,12 +4,10 @@ export default class Verse implements IVerse {
     public key;
     public number;
     public content;
-    public bibleReferences;
 
     constructor(i: IVerse) {
         this.key = i.key;
         this.number = i.number;
         this.content = i.content;
-        this.bibleReferences = i.bibleReferences;
     }
 }
