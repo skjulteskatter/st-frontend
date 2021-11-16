75235b2e8a1435d85deb54b6fe1ecd0345cf3077
Sigve Hansen <sighan12@gmail.com>
2021-11-16 08:42:09 +0100

    correctly implement IVerse in Verse

--- src/classes/scriptures/verse.ts
+++ src/classes/scriptures/verse.ts
@@ -4,10 +4,12 @@ export default class Verse implements IVerse {
     public key;
     public number;
     public content;
+    public bibleReferences;
 
     constructor(i: IVerse) {
         this.key = i.key;
         this.number = i.number;
         this.content = i.content;
+        this.bibleReferences = i.bibleReferences;
     }
 }
