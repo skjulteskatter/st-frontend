227c441280d2d10409411a919e9ff1c4ae8a7375
Sigve Hansen <sighan12@gmail.com>
2021-11-16 14:32:58 +0100

    add fake laoding bar for beter UX

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -4,8 +4,11 @@
 		leave-from-class="opacity-100"
 		leave-to-class="opacity-0"
 	>
-		<div v-if="loading" class="fixed inset-0 z-40 bg-white dark:bg-secondary">
-			<img class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-secondaryanimate-pulse h-20 drop-shadow-lg animate-pulse" src="/img/logo/dark.svg" alt="SongTreasures logo" height="80">
+		<div v-if="loading" class="fixed inset-0 z-40 bg-white flex flex-col justify-center items-center p-8">
+			<img class="h-20 drop-shadow-lg animate-pulse mb-10" src="/img/logo/dark.svg" alt="SongTreasures logo" height="80">
+			<div class="rounded-full h-1 max-w-sm w-full bg-gray-200 overflow-hidden">
+				<div class="loading-bar bg-primary h-full"></div>
+			</div>
 		</div>
 	</transition>
 </template>
@@ -26,3 +29,31 @@ export default class SplashScreen extends Vue {
 	public loading?: boolean;
 }
 </script>
+
+<style scoped>
+.loading-bar {
+	animation: loading-bar 2s linear;
+	animation-fill-mode: forwards;
+}
+
+@keyframes loading-bar {
+	0% {
+		width: 0%;
+	}
+	25% {
+		width: 25%;
+	}
+	40% {
+		width: 25%;
+	}
+	50% {
+		width: 50%;
+	}
+	75% {
+		width: 50%;
+	}
+	100% {
+		width: 90%;
+	}
+}
+</style>
