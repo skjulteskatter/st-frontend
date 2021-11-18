9d973210d86e589390629780de99430ed2c35de1
Fredrik Vedvik <fredrik@landax.no>
2021-11-18 13:41:27 +0100

    oof

--- translations/src/index.ts
+++ translations/src/index.ts
@@ -9,37 +9,43 @@ async function main() {
 
     for (const language of languages) {
         const path = "./translations/data/" + language;
-        const files = readdirSync(path);
 
-        for (const fileName of files) {
-            const content = readFileSync(path + "/" + fileName, {encoding: "utf-8"});
-            
-            const entries: {
-                [key: string]: string;
-            } = {};
+        try {
+            const files = readdirSync(path);
+
+            for (const fileName of files) {
+                const content = readFileSync(path + "/" + fileName, {encoding: "utf-8"});
+                
+                const entries: {
+                    [key: string]: string;
+                } = {};
+        
+                for (const line of content.split("\n")) {
+                    const data = line.split(";");
     
-            for (const line of content.split("\n")) {
-                const data = line.split(";");
-
-
-                if (data.length !== 3){
-                    continue;
-                }
     
-                try {
-                    const value = JSON.parse(data[1]);
-                    if (value) {
-                        entries[JSON.parse(data[0])] = value;
+                    if (data.length !== 3){
+                        continue;
+                    }
+        
+                    try {
+                        const value = JSON.parse(data[1]);
+                        if (value) {
+                            entries[JSON.parse(data[0])] = value;
+                        }
+                    }
+                    catch {
+                        //
                     }
                 }
-                catch {
-                    //
+    
+                if (Object.values(entries).length) {
+                    writeFileSync("./translations/out/" + language.split("-")[0] + ".json", JSON.stringify(entries));
                 }
             }
-
-            if (Object.values(entries).length) {
-                writeFileSync("./translations/out/" + language.split("-")[0] + ".json", JSON.stringify(entries));
-            }
+        }
+        catch {
+            // ignore errors;
         }
     }
 }
