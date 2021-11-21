b96afd77a3471199888dcfd155f3d069f0cccd74
Fredrik V <fredrik@vedvik.tech>
2021-11-21 16:17:28 +0100

    convert from json back to csv

--- translations/src/index.ts
+++ translations/src/index.ts
@@ -1,7 +1,7 @@
 import { existsSync, mkdirSync, readdirSync, readFileSync, statSync, writeFileSync } from "fs";
 import { join } from "path";
 
-async function main() {
+function main() {
     const languages = readdirSync("./translations/data/");
 
     if (!existsSync("./translations/out")) {
@@ -93,3 +93,36 @@ function getAllKeys() {
 }
 
 // getAllKeys();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function toCsv() {
+    if (!existsSync("./translations/out")) {
+        throw new Error("Need to make translations first");
+    }
+
+    if (!existsSync("./translations/out-csv")) {
+        mkdirSync("./translations/out-csv");
+    }
+
+    const files = readdirSync("./translations/out");
+
+    for (const file of files) {
+        const content = readFileSync("./translations/out/" + file, {encoding: "utf-8"});
+
+        const entries = JSON.parse(content) as {
+            [key: string]: string;
+        };
+
+        let csv = "";
+
+        for (const [key, value] of Object.entries(entries)) {
+            csv += `${JSON.stringify(key)},${JSON.stringify(value)},"","",""\n`;
+        }
+
+        if (csv.length) {
+            writeFileSync("./translations/out-csv/" + file.split(".")[0] + ".csv", csv);
+        }
+    }
+}
+
+toCsv();
