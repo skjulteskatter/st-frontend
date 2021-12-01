37d27360ece6a88169be16d92edd74212efb3e62
Fredrik Vedvik <fredrik@landax.no>
2021-12-01 09:48:39 +0100

    file cleanup

--- src/views/scriptures/books/chapters/index.ts
+++
@@ -1,7 +0,0 @@
-const ChapterView = () => import(/* webpackChunkName: 'scriptures' */ "./View.vue");
-const ChapterList = () => import(/* webpackChunkName: 'scriptures' */ "./List.vue");
-
-export {
-    ChapterList,
-    ChapterView,
-};
