271bfd81f64cbf4f239e5ef9402b73804e4e1db3
Sigve Hansen <sighan12@gmail.com>
2021-12-08 09:42:59 +0100

    improve translation selection a little bit

--- src/components/scriptures/SelectTranslation.vue
+++ src/components/scriptures/SelectTranslation.vue
@@ -1,35 +1,48 @@
 <template>
-    <BaseButton 
-        @click="show = !show" 
-        class="mb-2"
-    >
+    <button @click="show = !show">
         {{translation?.title ?? $t('common_translation')}}
-    </BaseButton>
+    </button>
     <Modal
         :show="show" 
         @close="show = false"
     >
-        <CheckboxList
-            v-if="checkboxData"
-            :items="checkboxData"
-            :title="$t('common_language')"
-            @change="checkboxData = checkboxData"
-        />
-        <div class="mb-2" v-for="translation in Translations" :key="translation.id">
-            <TranslationCard 
-                class="cursor-pointer"
-                :translation="translation" 
-                @click="setTranslation(translation)"
-            />
+        <div class="w-72">
+            <small class="opacity-50 uppercase text-xs tracking-wider mb-1">{{ $t("common_language") }}</small>
+            <BaseDropdown>
+                <template #button>
+                    <button class="px-3 py-2 rounded-md border border-black/20 flex gap-4 items-center">
+                        <span v-if="selectedLanguagesString">{{ selectedLanguagesString }}</span>
+                        <span v-else>Select language</span>
+                        <span class="opacity-50 text-sm" v-if="additionalSelections">{{ additionalSelections }}</span>
+                        <ChevronDownIcon class="w-5 h-5" />
+                    </button>
+                </template>
+                <CheckboxList
+                    v-if="checkboxData"
+                    :items="checkboxData"
+                    :title="$t('common_language')"
+                    @change="checkboxData = checkboxData"
+                />
+            </BaseDropdown>
+            <div class="flex flex-col gap-2 mt-4">
+                <template v-for="translation in Translations" :key="translation.id">
+                    <TranslationCard
+                        :translation="translation"
+                        @click="setTranslation(translation)"
+                    />
+                </template>
+            </div>
         </div>
     </Modal>
 </template>
+
 <script lang="ts">
 import { defineComponent, PropType } from "@vue/runtime-core";
 import Translation from "@/classes/scriptures/translation";
-import { CheckboxList } from "@/components/inputs";
+import { CheckboxList, BaseDropdown } from "@/components/inputs";
 import TranslationCard from "./TranslationCard.vue";
 import { BaseModal } from "@/components";
+import { ChevronDownIcon } from "@heroicons/vue/outline";
 
 type CheckboxData = {
     key: string;
@@ -41,8 +54,10 @@ export default defineComponent({
     name: "select-translation",
     components: {
         CheckboxList,
+        BaseDropdown,
         TranslationCard,
         Modal: BaseModal,
+        ChevronDownIcon,
     },
     props: {
         translation: {
@@ -91,6 +106,14 @@ export default defineComponent({
         },
         Translations() {
             return this.translations?.filter(i => this.FilterOnLanguages[i.language] === true) ?? [];
+        },
+        selectedLanguagesString() {
+            const strings = this.checkboxData.filter(c => c.value === true).map(c => c.label).slice(0, 2);
+            return strings.join(", ");
+        },
+        additionalSelections() {
+            const count = this.checkboxData.filter(c => c.value === true).length - 2;
+            return count > 0 ? `+${count} more` : "";
         },
     },
     methods: {
--- src/components/scriptures/TranslationCard.vue
+++ src/components/scriptures/TranslationCard.vue
@@ -1,12 +1,17 @@
 <template>
-    <BaseCard v-if="translation">
-        {{translation.title}}
-    </BaseCard>
+    <div v-if="translation" class="border cursor-pointer hover:border-secondary p-4 rounded-md flex gap-2">
+        <BookOpenIcon class="w-4 h-4 opacity-50 flex-shrink-0" />
+        <span class="text-sm">
+            <p>{{ translation.title }}</p>
+            <span class="opacity-50">{{ translation.language }}</span>
+        </span>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "@vue/runtime-core";
 import { Translation } from "@/classes/scriptures";
+import { BookOpenIcon } from "@heroicons/vue/outline";
 
 export default defineComponent({
     name: "translation-card",
@@ -14,6 +19,9 @@ export default defineComponent({
         translation: {
             type: Object as PropType<Translation>,
         },
+    },
+    components: {
+        BookOpenIcon,
     },
 });
 </script>
