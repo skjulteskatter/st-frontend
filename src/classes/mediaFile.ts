f3a9cf22113dbb9253a1adddf3df0caa0622ee4c
Fredrik Vedvik <fredrik@landax.no>
2022-01-11 17:18:10 +0100

    songs

--- src/classes/mediaFile.ts
+++ src/classes/mediaFile.ts
@@ -15,6 +15,7 @@ export default class MediaFile implements IMediaFile {
     public directUrl: string;
     public instrumentId?: string | undefined;
     public participants: Participant[];
+    public collectionIds: string[];
     
     constructor(i: IMediaFile) {
         this.id = i.id;
@@ -29,6 +30,7 @@ export default class MediaFile implements IMediaFile {
         this.directUrl = i.directUrl;
         this.instrumentId = i.instrumentId;
         this.participants = i.participants.map(i => new Participant(i));
+        this.collectionIds = i.collectionIds;
     }
 
     private _song: Song | null = null;
