7628a0ac463bbf3d85d5ddf695280df8126fd562
Fredrik Vedvik <fredrik@landax.no>
2022-01-28 13:15:47 +0100

    new version of cache

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -63,7 +63,7 @@ class CacheService {
         "books",
     ];
     // Only update if you need to clear cache for everyone or a new store is added.
-    private version = 33;
+    private version = 34;
 
     private db() {
         const v = this.version;
