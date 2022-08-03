224d77549f563bb7aa574c252b786489a579ad33
Fredrik Vedvik <fredrik@vedvik.tech>
2022-08-03 14:08:51 +0200

    tool for exporting credits

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -39,6 +39,20 @@
                     </BaseButton>
                 </template>
             </BaseCard>
+            <BaseCard class="col-span-2">
+                <template #header>
+                    Credits
+                </template>
+                <template #footer>
+                    <BaseButton
+                        @click="downloadCredits"
+                        :loading="loadingCreds"
+                        class="mb-2 float-right"
+                    >
+                        Download
+                    </BaseButton>
+                </template>
+            </BaseCard>
         </div>
     </div>
 </template>
@@ -52,6 +66,11 @@ import { Language } from "songtreasures";
 import BaseButton from "../components/BaseButton.vue";
 import Checkbox from "../components/inputs/Checkbox.vue";
 import BaseCard from "../components/BaseCard.vue";
+import songService from "@/services/songs/songService";
+import contributorService from "@/services/contributorService";
+import collectionService from "@/services/collectionService";
+import copyrightService from "@/services/songs/copyrightService";
+import { addAbortSignal } from "stream";
 
 type CustomLyrics = {
     [key: string]: {
@@ -75,6 +94,7 @@ export default defineComponent({
             includeLanguages: {} as {[key: string]: boolean},
             codepage: null as number | null,
             showCustomFiles: false,
+            loadingCreds: false,
         };
     },
     mounted() {
@@ -125,6 +145,80 @@ export default defineComponent({
                 this.loading = false;
             }
         },
+        async downloadCredits() {
+            this.loadingCreds = true;
+            const collections = await collectionService.list();
+            const songs = (await songService.list()).sort((a, b) => {
+                const cola = collections.find(c => a.collections.some(i => i.collectionId === c.id))
+                const colb = collections.find(c => b.collections.some(i => i.collectionId === c.id))
+
+                if (cola && colb && cola?.id !== colb?.id) {
+                    return colb?.priority - cola?.priority;
+                }
+
+                return (a.collections[0].number ?? 0) - (b.collections[0].number ?? 0)
+            });
+            const contributors = await contributorService.list();
+            const copyrights = await copyrightService.list();
+
+            let csv = "Collection;Number;Title;Author;Composer;Arranger;YearWritten;YearComposed;TextCopyright;MelodyCopyright\n";
+            for (const song of songs) {
+                csv += collections.find(c => song.collections[0].collectionId === c.id)?.key;
+                csv += ";"
+                csv += song.collections[0].number
+                csv += ";"
+                csv += "\"" + song.title.replace("\"", "\"\"") + "\""
+                csv += ";"
+
+                const getContributorCsv = (type: string) => {
+                    let conCsv = "\""
+                    const cons = song.participants.filter(p => p.type === type).map(i => contributors.find(c => c.id === i.contributorId)?.name ?? "")
+                    if (cons) {
+                        if (cons.length > 1) {
+                            conCsv += (cons.slice(0, cons.length - 2).join(", ") + " & " + cons[cons.length -1])?.replace("\"", "\"\"") ?? ""
+                        } else {
+                            conCsv += cons[0]?.replace("\"", "\"\"") ?? ""
+                        }
+                    }
+                    return conCsv + "\";"
+                }
+                csv += getContributorCsv("author")
+                csv += getContributorCsv("composer")
+                csv += getContributorCsv("arranger")
+                if (song.yearWritten) {
+                    csv += song.yearWritten + ";"
+                }
+                if (song.yearComposed) {
+                    csv += song.yearComposed + ";"
+                }
+                const textCopyright = song.copyrights.find(t => t.type === "text")
+                const melodyCopyright = song.copyrights.find(c => c.type === "melody")
+
+                csv += "\""
+                if (textCopyright) {
+                    csv += copyrights.find(i => i.id === textCopyright.referenceId)?.name.replace("\"", "\"\"")
+                }
+                csv += "\";"
+                csv += "\""
+                if (melodyCopyright) {
+                    csv += copyrights.find(i => i.id === melodyCopyright.referenceId)?.name.replace("\"", "\"\"")
+                }
+                csv += "\";"
+                csv += "\n"
+            }
+
+            const blob = new Blob([csv], {type: "text/csv"})
+            const a = document.createElement("a"), url = URL.createObjectURL(blob);
+            a.href = url;
+            a.download = `credits.csv`;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(() => {
+                document.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            });
+            this.loadingCreds = false;
+        }
     },
 });
 </script>
