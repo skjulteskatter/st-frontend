613893521bd02483458a00aa4151f0238c984ddc
Fredrik Vedvik <fredrik@vedvik.tech>
2023-01-25 19:45:09 +0100

    some styling

--- src/components/songs/SongInfoCard.vue
+++ src/components/songs/SongInfoCard.vue
@@ -87,34 +87,34 @@ if (props.song.image) {
                 </div>
                 <div class="text-gray-500 text-base flex flex-col gap-1 dark:text-gray-400" :class="{'hidden': !imageLoaded }">
                     <small
-                        class="flex gap-2"
+                        class="flex gap-1"
                         v-if="song.Authors.length"
                     >
                         <span>{{ (song.yearWritten ? $t("song_writtenInBy").replace('$year', song.yearWritten.toString()) : $t("song_writtenBy")).replace('$authors', '') }}</span>
                         <ContributorInfo :contributors="song.Authors" />
-                        <span v-if="song.yearPublished">(*{{song.yearPublished}})</span>
+                    <span v-if="song.yearPublished">({{ $t("song_publishedIn").replace('$year', song.yearPublished.toString()) }})</span>
                     </small>
                     <small
                         v-if="song.Composers.length"
-                        class="flex gap-2"
+                        class="flex gap-1"
                     >
                         <span>{{ (song.yearComposed ? $t("song_composedInBy").replace('$year', song.yearComposed.toString()) : $t("song_composedBy")).replace('$composers', '') }}</span>
                         <ContributorInfo :contributors="song.Composers" />
                     </small>
                     <small 
                         v-if="!song.Composers.length && !melodyOrigin"
-                        class="flex gap-2"
+                        class="flex gap-1"
                     >
                         <span>{{$t("song_unknownComposer")}}</span>
                     </small>
                     <small
-                        class="flex gap-2"
+                        class="flex gap-1"
                         v-if="melodyOrigin"
                     >
                         {{ melodyOrigin }}
                     </small>
                     <small
-                        class="flex gap-2"
+                        class="flex gap-1"
                         v-if="
                             song.copyright.melody &&
                             song.copyright.text &&
@@ -125,21 +125,21 @@ if (props.song.image) {
                     </small>
                     <div v-else>
                         <small
-                            class="flex gap-2"
+                            class="flex gap-1"
                             v-if="song.copyright.text"
                         >
-                            {{ $t("song_text") }} ©:
+                            {{ $t("song_text") }} ©
                             {{ song.copyright.text.getName(language) }}
                         </small>
                         <small
-                            class="flex gap-2"
+                            class="flex gap-1"
                             v-if="song.copyright.melody"
                         >
-                            {{ $t("song_melody") }} ©:
+                            {{ $t("song_melody") }} ©
                             {{ song.copyright.melody.getName(language) }}
                         </small>
                     </div>
-                    <small class="flex gap-2">
+                    <small class="flex gap-1">
                         <span v-if="song.originCountry">{{ song.originCountry }}</span>
                         <span v-if="song.originCountry">&middot;</span>
                         <span v-if="song.originalKey">{{ song.originalKey }}</span>
